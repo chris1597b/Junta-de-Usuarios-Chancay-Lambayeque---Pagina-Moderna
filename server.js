6f2a2a2cd3f0aca26c6fe8c9de12bb688caf6801
Christopher Bardales <91222178+chris1597b@users.noreply.github.com>
2026-06-17 10:26:54 -0500

    test1 - instalacion express para WEBHOOKS

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express";
+
+const app = express();
+
+app.get("/webhooks/facebook", (req, res) => {
+  const VERIFY_TOKEN = "Romacix_token";
+
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode && token) {
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
+      console.log("✅ Webhook verificado correctamente");
+      res.status(200).send(challenge);
+    } else {
+      res.status(403).send("Token inválido");
+    }
+  }
+});
+
+app.listen(3000, () => console.log("Servidor activo"));
